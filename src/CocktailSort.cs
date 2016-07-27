29c87ad6871ca84e2c457fea43a9991d56fa4b19
Daniel Imms <daimms@microsoft.com>
2016-07-26 18:51:05 -0700

    Add CocktailSort

---
+++ src/CocktailSort.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace GrowingWithTheWeb.Sorting
+{
+    public class CocktailSort : IGenericSortingAlgorithm
+    {
+        public void sort<T>(IList<T> list) where T : IComparable
+        {
+            int start = -1;
+            int end = list.Count - 2;
+            bool swapped;
+            
+            do {
+                swapped = false;
+                for (int i = ++start; i <= end; i++) {
+                    if (list[i].CompareTo(list[i + 1]) > 0) {
+                        swap(list, i, i + 1);
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped) {
+                    break;
+                }
+
+                swapped = false;
+                for (int i = --end; i >= start; i--) {
+                    if (list[i].CompareTo(list[i + 1]) > 0) {
+                        swap(list, i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        private void swap<T>(IList<T> list, int a, int b) where T : IComparable
+        {
+            T temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+    }
+}
