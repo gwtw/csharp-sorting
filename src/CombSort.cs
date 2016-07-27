e0f201385340e335795d26a30410fb284234ac93
Daniel Imms <daimms@microsoft.com>
2016-07-26 18:59:51 -0700

    Add CombSort

---
+++ src/CombSort.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace GrowingWithTheWeb.Sorting
+{
+    public class CombSort : IGenericSortingAlgorithm
+    {
+        public void sort<T>(IList<T> list) where T : IComparable
+        {
+            int gap = list.Count;
+            float shrinkFactor = 1.3f;
+            bool swapped = false;
+
+            while (gap > 1 || swapped) {
+                if (gap > 1) {
+                    gap = (int)(gap / shrinkFactor);
+                }
+
+                swapped = false;
+
+                for (int i = 0; gap + i < list.Count; i++) {
+                    if (list[i].CompareTo(list[i + gap]) > 0) {
+                        swap(list, i, i + gap);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+
+        private void swap<T>(IList<T> list, int a, int b) where T : IComparable
+        {
+            T temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+    }
+}
